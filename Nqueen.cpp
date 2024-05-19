ef56ef57a9cdc8181697e8c574a3970594613a30
Pritanshu69 <samantaprit1924@gmail.com>
2024-05-19 20:55:07 +0530

    n-queen

--- Nqueen.cpp
+++ Nqueen.cpp
@@ -1,28 +1,67 @@
 #include<iostream>
+
 using namespace std;
 
-int MAX_N = 10;
+#define N 8
 
-bool isSafe(int board[MAX_N][MAX_N], int row, int col, int N){
-    //NOTE - check weather the queen is in the same column or not
-    for(int i = 0; i < row; ++i){                       
-            if(board[i][col]== 1){
-                return false;
-            }
+bool isSafe(int board[N][N],int row, int col){
+    int i,j;
+
+    //NOTE - check this row on left side
+    
+    for(int i=0;i<col;i++){
+        if(board[row][i])
+        return false;
     }
-//NOTE - Now check for left diagonal 
-    for(int i=row,j=col;i>=0 && j>=0 ; --i,--j){
-        if(board[i][j]==1){
-            return false;
+
+    //NOTE - check upper diagonal on left side
+    for(i=row,j=col;i>=0 && j>=0; i--,j--)
+        if(board[i][j])
+        return false;
+
+    //NOTE - check lower diagonal on left side
+    for(i=row,j=col;j+=0 && i<N; i++,j--)
+        if(board[i][j])
+        return false;
+
+    return true;
+}
+
+bool solveNQUtil(int board[N][N], int col) {
+    if (col >= N)
+        return true;
+
+    for(int i=0; i<N; i++){
+        if(isSafe(board, i, col)){
+            board[i][col] = 1;
+
+        if(solveNQUtil(board,col+1))
+            return true;
+
+        board[i][col] = 0;
         }
     }
-//NOTE - Now check for right diagonal
-    for(int i=row,j=col;i>=0 && j<N; --i,++j){
-        if(board[i][j]==1){
-            return false;
-        }
+    return false;
+}
+
+bool solveNQ() {
+    int board[N][N] = {0};
+
+    if (solveNQUtil(board, 0) == false) {
+        cout << "Solution does not exist" << endl;
+        return false;
+    }
+
+    // Print solution
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << " " << board[i][j] << " ";
+        cout << endl;
     }
     return true;
 }
 
-bool solveNqueensUtil(int board[MAX_N][MAX_N],int row)
+int main() {
+    solveNQ();
+    return 0;
+}
