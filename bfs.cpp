d0629187f924a9131bf34b96f9ee1dfd1890cc85
Pritanshu69 <samantaprit1924@gmail.com>
2024-05-25 18:47:48 +0530

    bfs

--- bfs.cpp
+++ bfs.cpp
@@ -1,46 +1,47 @@
 #include<iostream>
+#include<fstream>
 #include<queue>
 
 using namespace std;
 
 const int MAX_VERTICES = 6;
 
-void bfs(int startVertex, int adj[MAX_VERTICES][MAX_VERTICES],int num){
-    bool visited[MAX_VERTICES]={false};
-    queue<int> q; //queue 
+void bfs(int startVertex,int adj[MAX_VERTICES][MAX_VERTICES],int num){
+    queue<int>q;
+    bool visited[MAX_VERTICES] = {false};
 
     visited[startVertex]=true;
     q.push(startVertex);
 
     while(!q.empty()){
-        int currentVertex = q.front();
+        int currenVertex =q.front();
         q.pop();
-        cout<<"Visited  "<<currentVertex<<endl;
+        cout<<"visited-> "<<currenVertex<<endl;
 
-        //explore all other vertices
         for(int adjVertex=0;adjVertex<num;++adjVertex){
-            if(adj[currentVertex][adjVertex]==1 && !visited[adjVertex]){
+            if(adj[currenVertex][adjVertex]!=1 && !visited[adjVertex]){
                 visited[adjVertex]=true;
                 q.push(adjVertex);
             }
         }
     }
 }
-
 int main(){
-    int adj[MAX_VERTICES][MAX_VERTICES]={
-        {0, 1, 1, 0, 0, 0}, // Vertex 0 is connected to Vertex 1 and 2
-        {1, 0, 0, 1, 1, 0}, // Vertex 1 is connected to Vertex 0, 3, and 4
-        {1, 0, 0, 0, 0, 1}, // Vertex 2 is connected to Vertex 0 and 5
-        {0, 1, 0, 0, 0, 0}, // Vertex 3 is connected to Vertex 1
-        {0, 1, 0, 0, 0, 1}, // Vertex 4 is connected to Vertex 1 and 5
-        {0, 0, 1, 0, 1, 0}  // Vertex 5 is connected to Vertex 2 and 4
-    };
-    int num = 6;
-    int startVertex = 0;
+    int adj[MAX_VERTICES][MAX_VERTICES];
+    int num=MAX_VERTICES;
 
-    cout<<"bfs from first"<<startVertex<<endl;
-      bfs(startVertex, adj, num);
+    ifstream inputFile("bfs.txt");
+    if(!inputFile){
+        cout<<"Error"<<endl;
+    }
+    for(int i=0;i<num;++i){
+        for(int j=0;j<num;++j){
+            inputFile>>adj[i][j];
+        }
+    }
+    inputFile.close();
 
-      return 0;
+    bfs(0,adj,num);
+    return 0;
+    // cout<<"BFS starting from vertex 0:\n"<<"visited-> 0\n"<<endl;
 }
